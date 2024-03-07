d86c5bd4c894407edda52e6fd73c8c43c8d60f25
tundeorekoya <tundeorekoya1998@gmail.com>
2024-03-07 12:12:59 -0800

    update

--- src/component/nav.jsx
+++ src/component/nav.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Stack } from "@mui/material"
+import {Link} from "@mui/material"
+import {logo}from "./utilities/constants"
 
-const nav = () => {
-  return (
-    <div>nav</div>
-  )
-}
+
+const nav = () => (
+  <Stack direction="row" alignItems="center" p={2} sx={{ justifyContent: "space-between", background: "#000", position:"sticky", top: 0  }}>
+    <Link
+      to="/"
+      style={{ display: "flex", alignItems: "center" }}
+    >
+      <img src={logo} alt="logo" height={45}/>
+    </Link>
+  </Stack>
+)
+
 
 export default nav
